281cb5767c636153826a7793ec16f99a833b0672
syafi-M <syafimq00@gmail.com>
2026-02-11 12:10:01 +0700

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import node from '@astrojs/node';
 
 import UnoCSS from 'unocss/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
   }),
   integrations: [
     UnoCSS({
